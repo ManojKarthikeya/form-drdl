e55871f041857aaea196e423f66ab356347c7955
ManojKarthikeya <manojkarthikeya@gmail.com>
2023-05-17 20:20:17 +0530

    added api

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,11 +2,26 @@ const express = require("express");
 const response = require("../models/response");
 const router = express.Router();
 
-router.post("/searchResponse", (req, res) => {});
+router.get("/response/user/:id", (req, res) => {
+	id = req.params.id;
+	response
+		.find({ userId: id })
+		.exec()
+		.then((responseObject) => {
+			res.send(JSON.stringify(responseObject));
+		});
+});
 
-router.post("/createResponse", (req, res) => {
+router.get("/response/:id", (req, res) => {
+	id = req.params.id;
+	response
+		.findById(id)
+		.exec()
+		.then((responseObject) => res.send(JSON.stringify(responseObject)));
+});
+
+router.post("/response", (req, res) => {
 	const { userId, fields } = req.body;
-	console.log(req.body);
 	response.create({
 		userId: userId,
 		fields: fields,
